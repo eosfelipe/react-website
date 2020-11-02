ab42c06750bfac17cccc5a4aeb91d520a92c716d
eosfelipe <escobedo.felipe@hotmail.com>
2020-11-02 12:50:14 -0600

    feat(pages): :sparkles: add page login

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import ScrollToTop from "../components/ScrollToTop";
+import SignIn from "../components/Signin";
 
 const SigninPage = () => {
   return (
-    <div>
-      <h1>Sign in page</h1>
-    </div>
-  )
-}
+    <>
+      <ScrollToTop />
+      <SignIn />
+    </>
+  );
+};
 
-export default SigninPage
+export default SigninPage;
